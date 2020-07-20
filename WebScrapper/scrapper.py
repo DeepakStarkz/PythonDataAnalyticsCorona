9bd2e936cb42c7757ea5542b431b6720d4be13d0
Deepak Starkz <deepakstarkz4@gmail.com>
2020-07-20 06:43:28 +0530

    Update scrapper.py

--- WebScrapper/scrapper.py
+++ WebScrapper/scrapper.py
@@ -1,7 +1,8 @@
 import requests
 import selenium 
-
+date="14 July 2020"
 url="https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Tamil_Nadu#covid19-container"
 data=requests.get(url)
-capturedData=data.split("By_district")[1]
-print(capturedData)
+capturedData=data.text.split("The data below are based on Health and Family Welfare Department of Tamil Nadu daily reports.")[1].split(">By cluster<")[0]
+capturedData2=capturedData.split(date)
+print(capturedData2[0])
